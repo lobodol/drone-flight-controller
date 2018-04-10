12f42a14bdf283fc714cb743d4960b15ff631fa2
lobodol <grobodol@gmail.com>
2018-04-10 18:35:14 +0200

    Add comments for maintainability

--- drone.ino
+++ drone.ino
@@ -126,28 +126,32 @@ void loop() {
 /**
  * Generate servo-signal on digital pins #4 #5 #6 #7 with a frequency of 250Hz (4ms period).
  * Direct port manipulation is used for performances.
- * 
+ *
  * This function might not take more than 2ms to run, which lets 2ms remaining to do other stuff.
- * 
- * @see https://www.arduino.cc/en/Reference/PortManipulation
- * 
+ *
+ * @see https:// www.arduino.cc/en/Reference/PortManipulation
+ *
  * @return void
  */
 void applyMotorSpeed() {
+    // Refresh rate is 250Hz: send ESC pulses every 4000µs
     while ((now = micros()) - loop_timer < 4000);
 
+    // Update loop timer
     loop_timer = now;
 
+    // Set pins #4 #5 #6 #7 HIGH
     PORTD |= B11110000;
 
+    // Wait until all pins #4 #5 #6 #7 are LOW
     while (PORTD >= 16) {
-        now = micros();
+        now        = micros();
         difference = now - loop_timer;
 
-        if (difference >= pulse_length_esc1) PORTD &= B11101111; // Pin 4
-        if (difference >= pulse_length_esc2) PORTD &= B11011111; // pin 5
-        if (difference >= pulse_length_esc3) PORTD &= B10111111; // pin 6
-        if (difference >= pulse_length_esc4) PORTD &= B01111111; // pin 7
+        if (difference >= pulse_length_esc1) PORTD &= B11101111; // Set pin #4 LOW
+        if (difference >= pulse_length_esc2) PORTD &= B11011111; // Set pin #5 LOW
+        if (difference >= pulse_length_esc3) PORTD &= B10111111; // Set pin #6 LOW
+        if (difference >= pulse_length_esc4) PORTD &= B01111111; // Set pin #7 LOW
     }
 }
 
