40be9a1680c4e337ff94365b65056c8cdaa4cbb7
lobodol <grobodol@gmail.com>
2018-05-07 00:09:47 +0200

    Use const instead of hard coded value

--- drone.ino
+++ drone.ino
@@ -160,18 +160,21 @@ void applyMotorSpeed() {
  * @return void
  */
 void readSensor() {
-    Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
-    Wire.write(0x3B);                                                    //Send the requested starting register
-    Wire.endTransmission();                                              //End the transmission
-    Wire.requestFrom(0x68,14);                                           //Request 14 bytes from the MPU-6050
-    while(Wire.available() < 14);                                        //Wait until all the bytes are received
-    acc_x = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_x variable
-    acc_y = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_y variable
-    acc_z = Wire.read()<<8|Wire.read();                                  //Add the low and high byte to the acc_z variable
-    temperature = Wire.read()<<8|Wire.read();                            //Add the low and high byte to the temperature variable
-    gyro_x = Wire.read()<<8|Wire.read();                                 //Add the low and high byte to the gyro_x variable
-    gyro_y = Wire.read()<<8|Wire.read();                                 //Add the low and high byte to the gyro_y variable
-    gyro_z = Wire.read()<<8|Wire.read();                                 //Add the low and high byte to the gyro_z variable
+    Wire.beginTransmission(MPU_ADDRESS); // Start communicating with the MPU-6050
+    Wire.write(0x3B);                    // Send the requested starting register
+    Wire.endTransmission();              // End the transmission
+    Wire.requestFrom(MPU_ADDRESS,14);    // Request 14 bytes from the MPU-6050
+
+    // Wait until all the bytes are received
+    while(Wire.available() < 14);
+
+    acc_x  = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the acc_x variable
+    acc_y  = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the acc_y variable
+    acc_z  = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the acc_z variable
+    temperature = Wire.read() << 8 | Wire.read(); // Add the low and high byte to the temperature variable
+    gyro_x = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the gyro_x variable
+    gyro_y = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the gyro_y variable
+    gyro_z = Wire.read() << 8 | Wire.read();      // Add the low and high byte to the gyro_z variable
 }
 
 /**
@@ -356,17 +359,17 @@ void configureChannelMapping() {
  */
 void setupMpu6050Registers() {
     //Activate the MPU-6050
-    Wire.beginTransmission(0x68);             //Start communicating with the MPU-6050
+    Wire.beginTransmission(MPU_ADDRESS);             //Start communicating with the MPU-6050
     Wire.write(0x6B);                         //Send the requested starting register
     Wire.write(0x00);                         //Set the requested starting register
     Wire.endTransmission();                   //End the transmission
     //Configure the accelerometer (+/-8g)
-    Wire.beginTransmission(0x68);             //Start communicating with the MPU-6050
+    Wire.beginTransmission(MPU_ADDRESS);             //Start communicating with the MPU-6050
     Wire.write(0x1C);                         //Send the requested starting register
     Wire.write(0x10);                         //Set the requested starting register
     Wire.endTransmission();                   //End the transmission
     //Configure the gyro (500dps full scale)
-    Wire.beginTransmission(0x68);             //Start communicating with the MPU-6050
+    Wire.beginTransmission(MPU_ADDRESS);             //Start communicating with the MPU-6050
     Wire.write(0x1B);                         //Send the requested starting register
     Wire.write(0x08);                         //Set the requested starting register
     Wire.endTransmission();                   //End the transmission
